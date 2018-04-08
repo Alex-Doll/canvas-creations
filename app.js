8a1841da6ea5ca8f428f6eaa5a1c26e4df04aac5
Alex <doll3098@ou.edu>
2018-04-08 03:58:03 +0000

    show/hide auth link on navbar appropriately

--- app.js
+++ app.js
@@ -29,6 +29,11 @@ passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 
 
 
