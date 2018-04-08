cf7b4428da37a6828665ef7836ac8c0324c0495e
Alex <doll3098@ou.edu>
2018-04-08 04:45:29 +0000

    install and configure method-override

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var passport = require("passport");
 var localStrategy = require("passport-local");
+var methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 mongoose.connect("mongodb://localhost/canvas_creations");
 
 app.use(require("express-session")({
