de779c703a5dbdf9d31c44b10267fb0aeab7b9f9
Alex <doll3098@ou.edu>
2018-04-08 04:41:44 +0000

    allow only logged in users to submit new creation

--- models/creation.js
+++ models/creation.js
@@ -4,6 +4,13 @@ var creationSchema = new mongoose.Schema({
     title: String,
     image_url: String,
     website_url: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
     comments: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Comment"
--- routes/creations.js
+++ routes/creations.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var Creation = require("../models/creation");
 
 router.get("/creations", function(req, res) {
-    console.log(req.user);
     Creation.find({}, function(err, allCreations) {
         if (err) {
             console.log(err);
@@ -13,16 +12,19 @@ router.get("/creations", function(req, res) {
     });
 });
 
-router.get("/creations/new", function(req, res) {
+router.get("/creations/new", isLoggedIn, function(req, res) {
     res.render("creations/new");
 });
 
-router.post("/creations", function(req, res) {
+router.post("/creations", isLoggedIn, function(req, res) {
     Creation.create(req.body.creation, function(err, newCreation) {
         if (err) {
             console.log(err);
             res.redirect("/creations/new");
         } else {
+            newCreation.author.id = req.user._id;
+            newCreation.author.username = req.user.username;
+            newCreation.save()
             res.redirect("/creations");
         }
     });
@@ -39,5 +41,12 @@ router.get("/creations/:creation_id", function(req, res) {
     });
 });
 
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.redirect("/login");
+    }
+}
 
 module.exports = router;
